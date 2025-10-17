d350a686087b09aca35099db67ca7fdd1f5ef563
Naveen Kumar Kosari <naveenkumarkosari@gmail.com>
2025-10-17 22:36:32 +0530

    port

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	fmt.Println("init")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = string("8080")
+	}
+	fmt.Println("server started at port", port)
 }
